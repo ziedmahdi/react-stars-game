aa9b1f1a2fa0a7222f5582d8935f157bb188cb22
Feki Zied <feki.zied@perfectsoft.one>
2019-12-28 15:01:21 +0100

    create button component

---
+++ src/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Button = (propos) => {
+    return (   
+        <button className="semantic ui button">{propos.number}</button>
+    ) 
+}
+
+export default Button; 
